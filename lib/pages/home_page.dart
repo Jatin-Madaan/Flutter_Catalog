f410861cabcdcbf5197c8c2242fed60119290bc4
Jatin <jatinmadaan9882@gmail.com>
2021-12-26 18:57:32 +0530

    Drawer header and List View added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/widgets/drawer.dart';
 
 class HomePage extends StatelessWidget{
   int days = 30;
@@ -16,7 +17,7 @@ class HomePage extends StatelessWidget{
           child: Text("welcome to $days of $name"),
         ),
       ),
-      drawer: Drawer(),
+      drawer: MyDrawer(),
     );
   }
 }
